98c80347d51a1148dec2d75cb55040565a49b4ac
Dat <todat2001@gmail.com>
2022-09-07 11:34:15 +0700

    add basic winner to board.js

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -8,20 +8,51 @@ const Board = (props) => {
     xIsNext: true,
   });
 
+  const checkWinner = (squares) => {
+    const linesOfWin = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < linesOfWin.length; i++) {
+      const [a, b, c] = linesOfWin[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  let status = checkWinner(squareIsClick.squares)
+    ? `Winner: ${checkWinner(squareIsClick.squares)}`
+    : `Player: ${squareIsClick.xIsNext ? "X" : "O"}`;
+
   const onClickHandler = (index) => {
+    // vì react không sửa được trực tiếp mà nên để func set... với useState
     const squares = squareIsClick.squares.slice();
+    if (checkWinner(squares) || squares[index]) {
+      return;
+    }
+
     squares[index] = squareIsClick.xIsNext ? "X" : "O";
-    console.log(squareIsClick.squares);
     setSquareIsClick({ squares: squares, xIsNext: !squareIsClick.xIsNext });
   };
   const arr = [];
   for (let i = 0; i < 9; i++) {
     arr.push(i);
   }
-  let h1 = `Player: ${squareIsClick.xIsNext ? "X" : "O"}`;
   return (
     <React.Fragment>
-      <h1>{h1}</h1>
+      <h1>{status}</h1>
       <div className={classes.board}>
         {arr.map((index) => (
           <Square
